aacf87752da10f04c4dcdcb91948fec2988e3ac7
newmanshee <newman_shee@hotmail.com>
2016-12-04 14:15:33 -0500

    make it unit testable

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,14 @@
 public class HelloWorld
 {
+  public static final String MESSAGE = "Hello World";
+  
   public static void main(String[] argv)
   {
-    System.out.println("Hello World");
+    System.out.println(getMessage());
+  }
+  
+  public String getMessage()
+  {
+    return MESSAGE;
   }
 };
